f91c4743f70efdf935939d614b2bf4ddb848265c
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-10-05 11:13:11 +0500

    add multiple manifest versions support

--- build.ts
+++ build.ts
@@ -1,9 +1,23 @@
 import { cp, rm, exists } from 'fs/promises'
+import path from 'path'
 import vuePlugin from '@eckidevs/bun-plugin-vue'
 import styleLoader from 'bun-style-loader'
 
-const STATIC_FILES_DIRECTORY = './public/'
+const MANIFEST_VERSION = process.env.BIND_MANIFEST_VERSION
+
+if (MANIFEST_VERSION !== 'v3' && MANIFEST_VERSION !== 'v2') {
+    throw new Error(
+        'MANIFEST_VERSION environment variable must be specified with value "v2" or "v3"',
+    )
+}
+
 const OUT_DIRECTORY = './dist'
+
+const STATIC_FILES_DIRECTORY = './public/'
+const MANIFEST_FILES_PATHS = {
+    v2: './manifest-v2.json',
+    v3: './manifest-v3.json',
+}
 
 if (await exists(OUT_DIRECTORY)) {
     await rm(OUT_DIRECTORY, { recursive: true })
@@ -33,6 +47,12 @@ Bun.build({
                     await cp(STATIC_FILES_DIRECTORY, bun.config.outdir, {
                         recursive: true,
                     })
+
+                    console.log(`Copying manifest ${MANIFEST_VERSION} to the bundle`)
+                    await cp(
+                        MANIFEST_FILES_PATHS[MANIFEST_VERSION],
+                        path.resolve(bun.config.outdir, 'manifest.json'),
+                    )
                 })
             },
         },
